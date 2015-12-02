3c01d0a86aabdc81f7556565c1bb4ecf99492ccb
Remco Tolsma <info@remcotolsma.nl>
2015-12-02 09:48:11 +0100

    Added comment header.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Title: iDEAL Advanced abstract integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 abstract class Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function get_config_factory_class() {
 		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_ConfigFactory';
